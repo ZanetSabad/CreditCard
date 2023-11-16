9a4b120ef68ed09ceca2c0274032f5b8d4a550a9
Žaneta Sabadka <zaneta.sabadkova@gmail.com>
2023-11-16 19:30:11 +0100

    začátek componenty CreditCard

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './index.css'
+import CreditCard from "./components/CreditCard";
 
 const App = () => {
   return (
     <div>
       <h2>Zde prosím zadejte údaje Vaší kreditní karty: </h2>
+      <CreditCard />
     </div>
   )
 }
---
+++ src/components/CreditCard/index.js
@@ -0,0 +1,17 @@
+import "./style.css"
+import { useRef, useEffect } from "react"
+
+const CreditCard = () => {
+  return (
+    <form>
+        <div>
+            <input type="text" /> 
+            <input type="text" /> 
+            <input type="text" /> 
+            <input type="text" /> 
+        </div>
+    </form>
+  )
+}
+
+export default CreditCard
